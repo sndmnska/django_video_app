a928cc3682c180f17d724d80a171cf143eaa7304
sndmnska <102878282+sndmnska@users.noreply.github.com>
2024-04-13 20:50:12 -0500

    inline videos

--- video_collection/views.py
+++ video_collection/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from .forms import VideoForm
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from .models import Video
+from .forms import VideoForm
 
 def home(request):
     app_name = 'Fun Music Videos' 
@@ -13,12 +16,21 @@ def add(request):
     if request.method == 'POST':
         new_video_form = VideoForm(request.POST)
         if new_video_form.is_valid():
-            new_video_form.save()
-            messages.info(request, 'New video saved!')
-            # TODO success message or redirect to video list
-        else:
-            messages.warning(request, 'Please check the data entered')
-            return render(request, 'video_collection/add.html')
+            try:
+                new_video_form.save()
+                return redirect('video_list')
+                # messages.info(request, 'New video saved!')
+                # TODO success message or redirect to video list
+            except ValidationError:
+                messages.warning(request, 'Invalid YouTube URL')
+            except IntegrityError:
+                messages.warning(request, 'You already added that video')
+        messages.warning(request, 'Please check the data entered')
+        return render(request, 'video_collection/add.html')
     
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+def video_list(request):
+    videos = Video.objects.all()
+    return render(request, 'video_collection/video_list.html', {'videos': videos})
